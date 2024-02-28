d4ad4c73ec7ccb3ec1ec94a5f4f1aa7ef2226065
Vitaliy Novichenko <Novichenkovitaliy@gmail.com>
2024-02-28 23:48:34 +0300

    Initial Commit

---
+++ EdutainmentKids/EdutainmentKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentKidsApp.swift
+//  EdutainmentKids
+//
+//  Created by Vitaliy Novichenko on 28.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
